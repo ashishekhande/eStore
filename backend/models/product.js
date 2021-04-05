7f80664e718f8c7c3938df49cc970dada9393d7d
Ashish Ekhande <ashishekhande007@gmail.com>
2021-04-05 21:13:22 +0100

    Initial commit

---
+++ backend/models/product.js
@@ -0,0 +1,96 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+    trim: true,
+    maxLength: [150, "product name can't exceed 150 characters"],
+  },
+
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+    maxLength: [150, "product name can't exceed 150 characters"],
+  },
+
+  description: {
+    type: Number,
+    required: [true, "Please enter product description"],
+  },
+
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "Please select category"],
+    enum: {
+      values: [
+        "Electronics",
+        "Cameras",
+        "Laptops",
+        "Accessories",
+        "Headphones",
+        "Food",
+        "Books",
+        "Clothes/Shoes",
+        "Beauty/Health",
+        "Sports",
+        "Outdoor",
+        "Home",
+      ],
+      message: "Please select correct category for Product",
+    },
+  },
+  seller: {
+    type: String,
+    required: [true, "Please enter name of seller"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "Please enter how many products are currently in stock"],
+    maxLength: [5, "Product name can't exceed 5 characters"],
+    default: 0,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("product", productSchema);
